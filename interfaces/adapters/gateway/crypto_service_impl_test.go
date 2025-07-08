068e708806c6b9e64438de93357566fea42aa0a4
ktaki8ra <takihara@rabbix.jp>
2025-07-08 15:59:41 +0900

    fix: indent

--- interfaces/adapters/gateway/crypto_service_impl_test.go
+++ interfaces/adapters/gateway/crypto_service_impl_test.go
@@ -1,8 +1,8 @@
 package gateway_test
 
 import (
-	"testing"
-	"github.com/stretchr/testify/assert"
+    "testing"
+    "github.com/stretchr/testify/assert"
 
     "github.com/ktaki8ra/cleanarch-go/interfaces/adapters/gateway"
     "github.com/ktaki8ra/cleanarch-go/domain/domain_model"
@@ -12,7 +12,7 @@ func TestCryptoServiceImpl(t *testing.T) {
     t.Run("Success", func(t *testing.T) {
         secretKey := "RTw9EHjFBcdJfrNehvL3eTEDYkFCZXFg"
         plainTextPassword := domain_model.PlainTextPassword{Value: "p4ssw0rd"}
-        
+
         cs := &gateway.CryptoServiceImpl{SecretKey: secretKey}
 
         encryptedPassword, err := cs.Encrypt(plainTextPassword)
